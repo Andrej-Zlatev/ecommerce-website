65035d2468918d8ffa4085aeb8e11eb9fd9fec78
Andrej Zlatev <andrej.zlatev31@gmail.com>
2024-10-07 23:48:23 +0200

    Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import HomeBanner from "./components/common/HomeBanner";
-import { Product } from "./types/product";
+// import { Product } from "./types/product";
 
-async function getProducts() {
-  const res = await fetch("http://localhost:3001/products");
-  return res.json();
-}
+// async function getProducts() {
+//   const res = await fetch("http://localhost:3001/products");
+//   return res.json();
+// }
 
 export default async function Home() {
-  const products = await getProducts();
+  // const products = await getProducts();
 
   // Filter products where 'new' is true
-  const newProducts = products.filter(
-    (product: Product) => product.new === true
-  );
+  // const newProducts = products.filter(
+  //   (product: Product) => product.new === true
+  // );
 
   return (
     <>
